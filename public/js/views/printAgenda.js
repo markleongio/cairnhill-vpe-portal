6b7c51453f4d2258486a9e626991c4e337f7f2ed
markleongio <123727652+markleongio@users.noreply.github.com>
2026-07-13 10:12:37 +0800

    Add files via upload

--- public/js/views/printAgenda.js
+++ public/js/views/printAgenda.js
@@ -217,24 +217,37 @@ function memberName(id) {
 
 // Builds the club's social channel icons (only the ones actually configured
 // in Club Settings). Used beside the QR code in print, and again on the
-// public post-scan view.
+// public post-scan view. Uses inline SVG rather than an icon font so it
+// always renders correctly regardless of whether a CDN icon font loaded.
+const SOCIAL_ICON_PATHS = {
+  youtube_url: 'M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z',
+  facebook_url: 'M9.101 23.691v-7.98H6.627v-3.667h2.474v-1.58c0-4.085 1.848-5.978 5.858-5.978.401 0 .955.042 1.468.103a8.68 8.68 0 0 1 1.141.195v3.325a8.623 8.623 0 0 0-.653-.036 26.805 26.805 0 0 0-.733-.009c-.707 0-1.259.096-1.675.309a1.686 1.686 0 0 0-.679.622c-.258.42-.353.995-.353 1.752v1.297h3.919l-.386 1.913-.287 1.754h-3.246v8.245C19.396 23.238 24 18.179 24 12.044c0-6.628-5.373-12-12-12s-12 5.372-12 12c0 6.135 4.604 11.194 10.101 11.647',
+  instagram_url: 'M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zm0 10.162a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.88 1.44 1.44 0 0 0 0-2.88z',
+  linkedin_url: 'M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z',
+  tiktok_url: 'M16.6 5.82s.51.5 0 0A4.278 4.278 0 0 1 15.54 3h-3.09v12.4a2.592 2.592 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6 0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64 0 3.33 2.76 5.7 5.69 5.7 3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48z',
+};
 function buildSocialLinksHtml(clubSocial) {
   clubSocial = clubSocial || {};
   const platforms = [
-    ['youtube_url', 'ti-brand-youtube', 'YouTube'],
-    ['facebook_url', 'ti-brand-facebook', 'Facebook'],
-    ['instagram_url', 'ti-brand-instagram', 'Instagram'],
-    ['linkedin_url', 'ti-brand-linkedin', 'LinkedIn'],
-    ['tiktok_url', 'ti-brand-tiktok', 'TikTok'],
+    ['youtube_url', 'YouTube'],
+    ['facebook_url', 'Facebook'],
+    ['instagram_url', 'Instagram'],
+    ['linkedin_url', 'LinkedIn'],
+    ['tiktok_url', 'TikTok'],
   ];
   const icons = platforms
     .filter(function (p) { return clubSocial[p[0]]; })
     .map(function (p) {
-      return '<a href="' + escapeHtml(clubSocial[p[0]]) + '" target="_blank" class="social-icon" title="' + p[2] + '"><i class="ti ' + p[1] + '"></i></a>';
+      return '<a href="' + escapeHtml(clubSocial[p[0]]) + '" target="_blank" class="social-icon" title="' + p[1] + '">' +
+        '<svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="' + SOCIAL_ICON_PATHS[p[0]] + '"></path></svg>' +
+      '</a>';
     })
     .join('');
   if (!icons) return '';
-  return '<div class="social-links">' + icons + '</div>';
+  return '<div class="social-links-block">' +
+    '<div class="social-links-title">Subscribe 点赞</div>' +
+    '<div class="social-links">' + icons + '</div>' +
+  '</div>';
 }
 
 // Splits free-text like "邱兰英 L5分区总监" into { name: "邱兰英", designation: "L5分区总监" }
